492812f4428ee27873b02b3fdeac0996c24ac7bc
Kastriot <krasniqikastriot01@gmail.com>
2022-02-16 16:21:02 +0100

    -m

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $users = User::all();
